19c9be4dfa779a33d5ecfb0469950dcfc04e7de4
genieAction <66431034+genieAction@users.noreply.github.com>
2020-07-30 20:58:32 +0300

    add validations types to imports

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const validate = require('./src/validator/validator')
+const validations = require('./src/validator/validationTypes')
 const sanitize = require('./src/sanitizer/sanitizer')
 const sanitizers = require('./src/sanitizer/sanitizerTypes')
 
 module.exports = {
     validate,
+    validations,
     sanitize,
     sanitizers
 }
